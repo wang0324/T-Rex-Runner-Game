06e55927b49b1a1167baa843d088f7c512240a0e
joeandbob <32173924+joeandbob@users.noreply.github.com>
2018-03-01 11:10:21 -0800

    The actual game

---
+++ Game.java
@@ -0,0 +1,20 @@
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class Game extends PApplet {
+	Cactus jeff;
+	
+	public void setup() {
+		size(800, 400);
+		
+		jeff = new Cactus(400, 200, this, "../assets/cactus1.png");
+		
+	}
+	
+	public void draw() {
+		background(255);
+		
+		jeff.draw();
+		
+	}
+}
